df20a5ccf1679523bef72c67917177a2d9d619a6
ogh826 <eys555@naver.com>
2024-07-22 15:29:53 +0900

    Class Character
    객체지향과 클래스 과제

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Class_07._22
+{
+    internal class Program
+    {
+        class Character
+        {
+            int level;
+            int hp;
+            float speed;
+            int atk;
+
+            void Foward()
+            {
+
+            }
+
+            void Back()
+            {
+
+            }
+
+            void Left()
+            {
+
+            }
+
+            void Right()
+            {
+
+            }
+
+            void Attack()
+            {
+
+            }
+        
+            void Damage()
+            {
+
+            }
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
